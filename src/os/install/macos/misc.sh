b0c092429c2e349dffe8f84e48d2af553c18ece5
Mark C Mitchell <mark@markcmitchell.net>
2016-08-08 16:12:59 +1000

    Add macos install Autojump

--- src/os/install/macos/misc.sh
+++ src/os/install/macos/misc.sh
@@ -10,6 +10,10 @@ install_apps() {
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+    brew_install "Autojump" "autojump"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
     brew_install "Chrome" "google-chrome" "caskroom/cask" "cask"
     brew_install "Chrome Canary" "google-chrome-canary" "caskroom/versions" "cask"
     brew_install "Chromium" "chromium" "caskroom/cask" "cask"
