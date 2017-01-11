7ee48eaf6c49a5aa1fd878f5d9289c083ab50866
Mark C Mitchell <mark@markcmitchell.net>
2017-01-11 18:51:57 +1100

    add script for setting up vim plugins and tmux config on a Raspberry Pi. Very ad-hoc atm, will encorporate better later when I have time to test

---
+++ src/raspberry_pi.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "utils.sh"
+
+echo -e "Install vim plugins, and tmux config\n"
+echo -e "This repo should be in ~/dotfiles"
+
+touch ~/.bash.local
+
+ln -s ~/dotfiles/src/vim/vimrc ~/.vimrc
+
+# should successfully install all vim plugins.
+cd os/install && ./vim.sh
+
+ln -s ~/dotfiles/src/shell/tmux.conf ~/.tmux.conf
+
+echo -e "All done. You may have to edit ~/.tmux.conf to remove some commands the OS complains about.\n"
+
+echo -e "For mouse mode, try: \n"
+echo -e "setw -g mode-mouse on \n"
+echo -e "set -g mouse-select-pane on \n"
+echo -e "set -g mouse-resize-pane on \n"
+echo -e "set -g mouse-select-window on \n"
