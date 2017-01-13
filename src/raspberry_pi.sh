2de73c0ae49fe01eb6af1459038fcc99f8c8003a
Mark C Mitchell <mark@markcmitchell.net>
2017-01-13 23:09:56 +1100

    change echo -e's to printf

--- src/raspberry_pi.sh
+++ src/raspberry_pi.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-echo -e "Note: This repo should be in ~/dotfiles! This is so soft links are linked correctly.\n"
 
 echo -e "Running sudo apt-get update\n"
 sudo apt-get update
 
 echo -e "Installing vim\n"
 sudo apt-get --yes --force-yes install vim-runtime vim
+printf "Note: This repo should be in ~/dotfiles! This is so soft links are linked correctly.\n"
 
-echo -e "Install vim plugins, and tmux config\n"
+printf "Install vim plugins, and tmux config\n"
 
 touch ~/.bash.local
 
@@ -18,10 +18,10 @@ cd os/install && ./vim.sh
 
 ln -s ~/dotfiles/src/shell/tmux.conf ~/.tmux.conf
 
-echo -e "All done. You may have to edit ~/.tmux.conf to remove some commands the OS complains about.\n"
+printf "All done. You may have to edit ~/.tmux.conf to remove some commands the OS complains about.\n"
 
-echo -e "For mouse mode, try: \n"
-echo -e "setw -g mode-mouse on \n"
-echo -e "set -g mouse-select-pane on \n"
-echo -e "set -g mouse-resize-pane on \n"
-echo -e "set -g mouse-select-window on \n"
+printf "For mouse mode, try: \n"
+printf "setw -g mode-mouse on \n"
+printf "set -g mouse-select-pane on \n"
+printf "set -g mouse-resize-pane on \n"
+printf "set -g mouse-select-window on \n"
