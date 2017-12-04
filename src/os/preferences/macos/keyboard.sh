34996700292b34e2495122c2bce28a0c60caeb56
Mitchell, Mark <mark.mitchell@fjordnet.com>
2017-12-04 18:18:44 +1100

    [macos] Disable more automatic text substitutions

--- src/os/preferences/macos/keyboard.sh
+++ src/os/preferences/macos/keyboard.sh
@@ -24,3 +24,12 @@ execute "defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false" \
 
 execute "defaults write -g NSAutomaticDashSubstitutionEnabled -bool false" \
     "Disable smart dashes"
+
+execute "defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false" \
+    "Disable automatic capitalization"
+
+execute "defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false" \
+    "Disable automatic correction"
+
+execute "defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool false" \
+    "Disable automatic period substitution"
