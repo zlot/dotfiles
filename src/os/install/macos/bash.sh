32a93faec88736ddb4a851000707c86eb37b7bbb
Mitchell, Mark <mark.mitchell@fjordnet.com>
2017-12-04 18:19:57 +1100

    [macos] Update install code for `bash-completion`
    * `bash-completion` v2 is now included in core Homebrew.
    * `homebrew-versions` has been deprecated.

--- src/os/install/macos/bash.sh
+++ src/os/install/macos/bash.sh
@@ -75,7 +75,7 @@ main() {
     brew_install "Bash" "bash" \
         && change_default_bash
 
-    brew_install "Bash Completion 2" "bash-completion2" "homebrew/versions"
+    brew_install "Bash Completion 2" "bash-completion@2"
 
 }
 
